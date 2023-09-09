5201dbf92a5fdd97d6652ce42c5565b42ce66db1
igorferreira <igorferreirabr@gmail.com>
2023-09-09 02:48:31 -0300

    Adicionado mais testes

--- src/test/java/com/github/igorferreira/otp/AppTest.java
+++ src/test/java/com/github/igorferreira/otp/AppTest.java
@@ -3,11 +3,13 @@ package com.github.igorferreira.otp;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
 class AppTest {
+
     /**
      * Rigorous Test.
      */
@@ -17,5 +19,13 @@ class AppTest {
         App app = new App();
         assertNotNull(app.generateOTP("B23743HKC446"));
     }
-}
 
+    @Test
+    void test_generateOTP_withValidSecret() {
+        App app = new App();
+        String secret = "validSecret";
+        String otp = app.generateOTP(secret);
+        assertNotNull(otp);
+        assertTrue(otp.matches("[0-9]{6}"));
+    }
+}
